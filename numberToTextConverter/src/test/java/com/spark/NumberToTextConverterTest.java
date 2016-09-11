04baa3c5f4f0cc74503ae5d70919e39355559e5b
GaneshArunagiri <garunagiri@sapient.com>
2016-09-11 13:20:46 +0530

    Initial Test case - Red

---
+++ numberToTextConverter/src/test/java/com/spark/NumberToTextConverterTest.java
@@ -0,0 +1,14 @@
+package com.spark;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class NumberToTextConverterTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
