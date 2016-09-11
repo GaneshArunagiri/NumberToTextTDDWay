7e8233448ed783f5cf21fe430a6fa1dcf21c86a5
GaneshArunagiri <garunagiri@sapient.com>
2016-09-11 14:17:32 +0530

    Added small functional code to converter. GREEN

--- numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
+++ numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
@@ -4,11 +4,17 @@ public class NumberToTextConverter {
 
 	private static final String[] numberNames = { "", " one", " two", " three", " four", " five", " six", " seven",
 			" eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen",
-			" seventeen", " eighteen", " nineteen"," twenty" };
+			" seventeen", " eighteen", " nineteen", " twenty" };
+	private static final String[] tensNames = { "", " ten", " twenty" };
 
 	public String convertNumbertToText(int inputNumber) {
 		String result = "";
-		result = numberNames[inputNumber];
+		if (inputNumber <= 20) {
+			result = numberNames[inputNumber];
+		} else {
+			inputNumber = inputNumber / 10;
+			result = tensNames[inputNumber] + " one";
+		}
 		return result.trim();
 	}
 
