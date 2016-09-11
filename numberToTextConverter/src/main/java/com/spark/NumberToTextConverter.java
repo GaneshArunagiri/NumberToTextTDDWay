d109bb6e5bb1fbf876e8ea27872733af37bcd0d0
GaneshArunagiri <garunagiri@sapient.com>
2016-09-11 14:27:05 +0530

    Refactor for clean code perspective.Removed hard coded values. GREEN

--- numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
+++ numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
@@ -4,16 +4,19 @@ public class NumberToTextConverter {
 
 	private static final String[] numberNames = { "", " one", " two", " three", " four", " five", " six", " seven",
 			" eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen",
-			" seventeen", " eighteen", " nineteen", " twenty" };
+			" seventeen", " eighteen", " nineteen" };
 	private static final String[] tensNames = { "", " ten", " twenty" };
-
+	private static final int NUMBER_TEN = 10;
+	private static final int NUMBER_TWENTY = 20;
+	
 	public String convertNumbertToText(int inputNumber) {
 		String result = "";
-		if (inputNumber <= 20) {
+		if (inputNumber < NUMBER_TWENTY) {
 			result = numberNames[inputNumber];
 		} else {
-			inputNumber = inputNumber / 10;
-			result = tensNames[inputNumber] + " one";
+			int tenthDigit = inputNumber / NUMBER_TEN;
+			int numberDigit = inputNumber % NUMBER_TEN;
+			result = tensNames[tenthDigit] + numberNames[numberDigit];
 		}
 		return result.trim();
 	}
