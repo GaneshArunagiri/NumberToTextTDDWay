0fa3e3606dcdc71266d5a3051b5e74526996ee4e
GaneshArunagiri <garunagiri@sapient.com>
2016-09-11 13:57:58 +0530

    Clean code Refactor.avoided redundant code with extract method.GREEN

--- numberToTextConverter/src/test/java/com/spark/NumberToTextConverterTest.java
+++ numberToTextConverter/src/test/java/com/spark/NumberToTextConverterTest.java
@@ -2,26 +2,37 @@ package com.spark;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class NumberToTextConverterTest {
 
+	private NumberToTextConverter converter;
+
+	@Before
+	public void setup() {
+		converter = new NumberToTextConverter();
+	}
+
 	@Test
 	public void testNumberOneToText() {
-		NumberToTextConverter converter=new NumberToTextConverter();
-		assertEquals("one",converter.convertNumbertToText(1));
+		assertEquals("one", converter.convertNumbertToText(1));
 	}
-	
+
 	@Test
 	public void testNumberTwoToText() {
-		NumberToTextConverter converter=new NumberToTextConverter();
-		assertEquals("two",converter.convertNumbertToText(2));
+		assertEquals("two", converter.convertNumbertToText(2));
 	}
-	
+
 	@Test
 	public void testNumberThreeToText() {
-		NumberToTextConverter converter=new NumberToTextConverter();
-		assertEquals("three",converter.convertNumbertToText(3));
+		assertEquals("three", converter.convertNumbertToText(3));
+	}
+
+	@After
+	public void cleanUp() {
+		converter = null;
 	}
 
 }
