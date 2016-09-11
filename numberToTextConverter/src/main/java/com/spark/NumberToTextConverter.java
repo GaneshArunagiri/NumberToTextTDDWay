6c7462c62502b55b14b52a1c66ca137eb7b12784
GaneshArunagiri <garunagiri@sapient.com>
2016-09-11 14:01:36 +0530

    Refactor converter.Identified the pattern to get value till twenty.GREEN

--- numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
+++ numberToTextConverter/src/main/java/com/spark/NumberToTextConverter.java
@@ -2,11 +2,14 @@ package com.spark;
 
 public class NumberToTextConverter {
 
-	private static final String[] numberNames={"","one","two","three"};
+	private static final String[] numberNames = { "", " one", " two", " three", " four", " five", " six", " seven",
+			" eight", " nine", " ten", " eleven", " twelve", " thirteen", " fourteen", " fifteen", " sixteen",
+			" seventeen", " eighteen", " nineteen" };
+
 	public String convertNumbertToText(int inputNumber) {
 		String result = "";
-		result=numberNames[inputNumber];
-		return result;
+		result = numberNames[inputNumber];
+		return result.trim();
 	}
 
 }
