0a13b2e9b959324f68a8673bffd6654f1ec0e284
GaneshArunagiri <garunagiri@sapient.com>
2016-09-15 18:06:15 +0530

    Single item B. Added small functional code. GREEN

--- orderTotalPrice/src/main/java/com/spark/checkout/OrderTotalPrice.java
+++ orderTotalPrice/src/main/java/com/spark/checkout/OrderTotalPrice.java
@@ -5,9 +5,9 @@ public class OrderTotalPrice {
 	public int calculatePrice(String products) {
 		int totalPrice=0;
 		if(products.equals("A"))
-		{
 			totalPrice= 50;
-		}
+		else if(products.equals("B"))
+			totalPrice=30;
 		return totalPrice;
 	}
 
