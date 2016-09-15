6ed8841d0c06c3c5e0a1860657e8811293ca2587
GaneshArunagiri <garunagiri@sapient.com>
2016-09-15 18:08:47 +0530

    Added cleanup in test class GREEN

--- orderTotalPrice/src/test/java/com/spark/checkout/OrderTotalPriceTest.java
+++ orderTotalPrice/src/test/java/com/spark/checkout/OrderTotalPriceTest.java
@@ -2,25 +2,40 @@ package com.spark.checkout;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrderTotalPriceTest {
 
+	private OrderTotalPrice ordTotal;
+
+	@Before
+	public void setup() {
+		ordTotal = new OrderTotalPrice();
+	}
+
 	@Test
 	public void testEmptyOrder() {
-		OrderTotalPrice ordTotal=new OrderTotalPrice();
+
 		assertEquals(0, ordTotal.calculatePrice(""));
 	}
- 
+
 	@Test
 	public void testSingleItemA() {
-		OrderTotalPrice ordTotal=new OrderTotalPrice();
+
 		assertEquals(50, ordTotal.calculatePrice("A"));
 	}
-	
+
 	@Test
 	public void testSingleItemB() {
-		OrderTotalPrice ordTotal=new OrderTotalPrice();
+
 		assertEquals(30, ordTotal.calculatePrice("B"));
+	}
+	
+	@After
+	public void cleanup()
+	{
+		ordTotal=null;
 	}
 }
