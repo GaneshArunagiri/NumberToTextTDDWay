484407cc3fb4e5437a7eac601cfe49aafa9ab87e
GaneshArunagiri <garunagiri@sapient.com>
2016-09-16 20:14:14 +0530

    Initial commit.Test File. RED

---
+++ rockScissorsPaper/src/test/java/com/spark/game/RockPaperScissorsGameTest.java
@@ -0,0 +1,14 @@
+package com.spark.game;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RockPaperScissorsGameTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
