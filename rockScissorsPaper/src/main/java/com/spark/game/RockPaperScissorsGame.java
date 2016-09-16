43e5e25d098905e32a9645fc3e02eb1b8f0b7b01
GaneshArunagiri <garunagiri@sapient.com>
2016-09-16 20:24:53 +0530

    small logic added to make the testCase success. GREEN

--- rockScissorsPaper/src/main/java/com/spark/game/RockPaperScissorsGame.java
+++ rockScissorsPaper/src/main/java/com/spark/game/RockPaperScissorsGame.java
@@ -3,7 +3,10 @@ package com.spark.game;
 public class RockPaperScissorsGame {
 
 	public String whoWins(String string, String string2) {
-		return "rock";
+		String winner="";
+		if(string.equals("rock")) winner="rock";
+		else winner="scissors";
+		return winner;
 	}
 
 }
