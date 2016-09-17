58735dc073f0deee9475444adbbcd1cc9242b060
GaneshArunagiri <garunagiri@sapient.com>
2016-09-17 21:21:30 +0530

    testAdd.AddedTwo methods add, contains.verify comments in testClass RED

--- linkedList/src/main/java/com/spark/collection/LinkedList.java
+++ linkedList/src/main/java/com/spark/collection/LinkedList.java
@@ -2,8 +2,17 @@ package com.spark.collection;
 
 public class LinkedList {
 	Node head;
+
 	public boolean isEmpty() {
-		return (head==null);
+		return (head == null);
+	}
+
+	public void add(String string) {
+
+	}
+
+	public boolean contains(String string) {
+		return false;
 	}
 
 }
--- linkedList/src/test/java/com/spark/collection/LinkedListTest.java
+++ linkedList/src/test/java/com/spark/collection/LinkedListTest.java
@@ -10,9 +10,22 @@ public class LinkedListTest {
 	public void testCreateLinkedListInstance() {
 		assertTrue(new LinkedList() instanceof LinkedList);
 	}
+
 	@Test
 	public void testCreateEmptyList() {
-		LinkedList list=new LinkedList();
+		LinkedList list = new LinkedList();
 		assertTrue(list.isEmpty());
+	}
+
+	@Test
+	public void testAddItemToList() {
+		/*Ideally there will be two test cases here. Will do it later.
+		 * 1- test only void add() - using Mockito doNothing() and verify(atleast 1 time) methods.
+		 * 2 -contains as a separate test case
+		 */
+		LinkedList list = new LinkedList();
+		list.add("ROCK");
+		assertTrue(list.contains("ROCK"));
+
 	}
 }
