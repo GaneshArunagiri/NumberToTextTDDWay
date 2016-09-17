7d0e4c81cff1d4763572cd59336bff57726c8e3a
GaneshArunagiri <garunagiri@sapient.com>
2016-09-17 22:20:03 +0530

    Adding 3 items failed. RED

--- linkedList/src/test/java/com/spark/collection/LinkedListTest.java
+++ linkedList/src/test/java/com/spark/collection/LinkedListTest.java
@@ -19,25 +19,38 @@ public class LinkedListTest {
 
 	@Test
 	public void testAddItemToList() {
-		/*Ideally there will be two test cases here. Will do it later.
-		 * 1- test only void add() - using Mockito doNothing() and verify(atleast 1 time) methods.
-		 * 2 -contains as a separate test case
+		/*
+		 * Ideally there will be two test cases here. Will do it later. 1- test
+		 * only void add() - using Mockito doNothing() and verify(atleast 1
+		 * time) methods. 2 -contains as a separate test case
 		 */
 		LinkedList list = new LinkedList();
 		list.add("ROCK");
 		assertTrue(list.contains("ROCK"));
 
 	}
-	
+
 	@Test
-	public void testAddTwoItemsToList()
-	{
-		//Same comment as above. Will use Mokito for add.
+	public void testAddTwoItemsToList() {
+		// Same comment as above. Will use Mokito for add.
 		LinkedList list = new LinkedList();
 		list.add("ROCK");
 		list.add("PAPER");
 		assertTrue(list.contains("ROCK"));
 		assertTrue(list.contains("PAPER"));
-		
+
+	}
+
+	@Test
+	public void testAddThreeItemsToList() {
+		// Same comment as above. Will use Mokito for add.
+		LinkedList list = new LinkedList();
+		list.add("ROCK");
+		list.add("PAPER");
+		list.add("SCISSORS");
+		assertTrue(list.contains("ROCK"));
+		assertTrue(list.contains("PAPER"));
+		assertTrue(list.contains("SCISSORS"));
+
 	}
 }
