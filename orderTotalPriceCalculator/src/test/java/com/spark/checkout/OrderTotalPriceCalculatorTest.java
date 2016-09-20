545344c5d4e2f5320894e769d591b7992a4f3e48
GaneshArunagiri <garunagiri@sapient.com>
2016-09-20 13:48:28 +0530

    Created Test class AssertionError: Not yet implemented. RED

---
+++ orderTotalPriceCalculator/src/test/java/com/spark/checkout/OrderTotalPriceCalculatorTest.java
@@ -0,0 +1,14 @@
+package com.spark.checkout;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class OrderTotalPriceCalculatorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
