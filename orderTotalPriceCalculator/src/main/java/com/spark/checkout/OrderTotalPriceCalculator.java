88642ee2b97b52d8adb470afae2efbdb1a69af43
GaneshArunagiri <garunagiri@sapient.com>
2016-09-20 14:01:08 +0530

    Single item A logic added. GREEN

--- orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
+++ orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
@@ -4,6 +4,8 @@ public class OrderTotalPriceCalculator {
 
 	public int calculateOrderTotal(String products) {
 		int totalPrice=0;
+		
+		if(products.equals("A")) totalPrice=50;
 		return totalPrice;
 	}
 
