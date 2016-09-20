097350677b857318b09e64347170403099beab0b
GaneshArunagiri <garunagiri@sapient.com>
2016-09-20 16:05:41 +0530

    Refactor . Removed the redundant code.

--- orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
+++ orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
@@ -13,23 +13,18 @@ public class OrderTotalPriceCalculator {
 
 	public int calculateOrderTotal(String products) {
 		int totalPrice = 0;
-		List<Item> ItemList=getProductList(products);
+		List<Item> ItemList = getProductList(products);
 
 		String product;
 		int qty = 0;
-		int price=0;
-		for (Item item:ItemList) {
+		int price = 0;
+		for (Item item : ItemList) {
 			product = item.getName();
-			price=item.getPrice();
+			price = item.getPrice();
 			qty = item.getItemQty();
-			if (product.equals("A"))
-				totalPrice += price * qty;
-			else if (product.equals("B"))
-				totalPrice += price * qty;
-			else if (product.equals("C"))
-				totalPrice += price * qty;
-			else if (product.equals("D"))
-				totalPrice += price * qty;
+
+			totalPrice += price * qty;
+
 		}
 		return totalPrice;
 	}
@@ -37,8 +32,8 @@ public class OrderTotalPriceCalculator {
 	private List<Item> getProductList(String products) {
 		List<String> productsList = Arrays.asList(products.split(""));
 		Set<String> uniqueProducts = new HashSet<String>(productsList);
-		
-		List<Item> itemList=new ArrayList<Item>();
+
+		List<Item> itemList = new ArrayList<Item>();
 		int quantity = 0;
 		for (String product : uniqueProducts) {
 			quantity = Collections.frequency(productsList, product);
