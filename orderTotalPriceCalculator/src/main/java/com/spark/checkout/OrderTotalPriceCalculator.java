8f1a59c24152e6cea074bdfb23ac6abb655ef444
GaneshArunagiri <garunagiri@sapient.com>
2016-09-20 14:09:56 +0530

    Refactor.SingleItem Pattern identified.C and D test case not req. GREEN

--- orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
+++ orderTotalPriceCalculator/src/main/java/com/spark/checkout/OrderTotalPriceCalculator.java
@@ -7,6 +7,8 @@ public class OrderTotalPriceCalculator {
 		
 		if(products.equals("A")) totalPrice=50;
 		else if(products.equals("B")) totalPrice=30;
+		else if(products.equals("C")) totalPrice=20;
+		else if(products.equals("D")) totalPrice=15;
 		return totalPrice;
 	}
 
--- orderTotalPriceCalculator/src/test/java/com/spark/checkout/OrderTotalPriceCalculatorTest.java
+++ orderTotalPriceCalculator/src/test/java/com/spark/checkout/OrderTotalPriceCalculatorTest.java
@@ -2,24 +2,35 @@ package com.spark.checkout;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class OrderTotalPriceCalculatorTest {
 
+	private OrderTotalPriceCalculator calculator;
+	@Before
+	public void setup()
+	{
+		 calculator= new OrderTotalPriceCalculator();
+	}
 	@Test
 	public void testEmptyOrder() {
-		OrderTotalPriceCalculator calculator= new OrderTotalPriceCalculator();
 		assertEquals(0, calculator.calculateOrderTotal(""));
 	}
 	@Test
 	public void testSingleItemA() {
-		OrderTotalPriceCalculator calculator= new OrderTotalPriceCalculator();
+		
 		assertEquals(50, calculator.calculateOrderTotal("A"));
 	}
 	@Test
 	public void testSingleItemB() {
-		OrderTotalPriceCalculator calculator= new OrderTotalPriceCalculator();
 		assertEquals(30, calculator.calculateOrderTotal("B"));
 	}
 
+	@After
+	public void cleanup()
+	{
+		calculator=null;
+	}
 }
